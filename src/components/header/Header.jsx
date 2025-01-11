3f33974263ed2dfbd188babb5261822c831c7c1b
ProlificCoder17 <fitasco@gmail.com>
2025-01-11 18:22:39 +0200

    Completed all components style implementation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,14 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hi there, I'm</h5>
+        <h1>Tatenda</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        
+      </div>
+    </header>
   )
 }
 
