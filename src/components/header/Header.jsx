2b61392c62c1919f4628593d38ce924770868266
Frost <fitasco@gmail.com>
2025-01-12 13:29:44 +0200

    Added profile image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/fiti.png'
+import HeaderSocial from './HeaderSocial'
 
 const Header = () => {
   return (
@@ -8,7 +11,13 @@ const Header = () => {
         <h5>Hi there, I'm</h5>
         <h1>Tatenda</h1>
         <h5 className="text-light">Fullstack Developer</h5>
-        
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="prolificCoder" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )
---
+++ src/components/header/HeaderSocial.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaLinkedin } from "react-icons/fa";
+
+const HeaderSocial = () => {
+  return (
+    <div className="header__socials">
+        <a href="https://linkedin.com"></a>
+        <a href="https://github.com"></a>
+        <a href="https://instagram"></a>
+        <FaLinkedin />
+    </div>
+  )
+}
+
+export default HeaderSocial
